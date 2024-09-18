7561f896127ef31b4aee29291f136bef72bcadc6
AsadJaved2 <asadjaved744@gmail.com>
2024-09-18 12:44:48 +0500

    Category Repository refactor

--- src/categories/cat.repository.ts
+++ src/categories/cat.repository.ts
@@ -2,12 +2,6 @@ import { DataSource, EntityManager, Repository } from 'typeorm';
 import { Category } from './cat.entity';
 import { Injectable } from '@nestjs/common';
 
-// export class CategoryRepository extends Repository<Category> {
-//   constructor(manager: EntityManager) {
-//     super(Category, manager);
-//   }
-// }
-
 @Injectable()
 export class CategoryRepository extends Repository<Category> {
   constructor(private dataSource: DataSource) {
